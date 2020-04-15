eff961cdd81e9d4afa3ac198efab3114e8c5b450
rliugoodlabs <richard24@gmail.com>
2020-04-15 16:37:47 -0400

    removing add filters by queries for now as it is not being used

--- src/main/java/src/main/java/testDriver/FieldFiltersDriver.java
+++ src/main/java/src/main/java/testDriver/FieldFiltersDriver.java
@@ -14,53 +14,21 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import src.main.java.ESQueryBuilder;
 import src.main.java.DashboardPage.KibanaNetworkDashBoardPage;
 
 public class FieldFiltersDriver {
 
 	private WebDriver driver;
 	private KibanaNetworkDashBoardPage page;
-	private ESQueryBuilder esQueryBuilder;
+
 	private WebDriverWait wait;
 	
 	public FieldFiltersDriver (WebDriver driver, KibanaNetworkDashBoardPage page) {
 		
-		this.esQueryBuilder = new ESQueryBuilder();
+
 		this.page = page;
 		this.driver = driver;
 		this.wait = new WebDriverWait(driver,500);
-	}
-	
-	public void setFieldFiltersByQuery (Map<String, String> filters) {
-		
-		WebElement addFilterButton = 
-				driver.findElement(page.getAddFilterButton());
-		addFilterButton.click();
-		
-		WebElement addQueryDSLButton = 
-				driver.findElement(page.getQueryDSLButton());
-		addQueryDSLButton.click();
-
-		WebElement codeBox = 
-				driver.findElement(page.getQueryDSLCodeBox());
-
-		codeBox.clear();
-
-		//Kibana does annoying auto-complete with bracets on queries which breaks
-		//direct insertion, so we have to paste our query
-		
-		pasteIntoTextBox (esQueryBuilder.buildQueryFromList(filters), codeBox, driver);
-
-	}
-	
-	private void pasteIntoTextBox (String text, WebElement textBox, WebDriver driver) {
-		
-		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-		StringSelection selection = new StringSelection(text);
-		clipboard.setContents(selection, selection);
-		textBox.sendKeys(Keys.chord(Keys.LEFT_CONTROL, "v"));
-
 	}
 	
 	public void setFieldFilters(Map<String, String> filters) {
