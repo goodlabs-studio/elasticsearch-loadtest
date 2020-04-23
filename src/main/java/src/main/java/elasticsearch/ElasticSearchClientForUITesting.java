510a11000ceff348ff0bbd026eb22d6603972001
rliugoodlabs <richard24@gmail.com>
2020-04-23 11:36:09 -0400

    changed es client to use https to do cache clear calls

--- src/main/java/src/main/java/elasticsearch/ElasticSearchClientForUITesting.java
+++ src/main/java/src/main/java/elasticsearch/ElasticSearchClientForUITesting.java
@@ -3,14 +3,32 @@ package src.main.java.elasticsearch;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.action.admin.indices.cache.clear.ClearIndicesCacheRequest;
 import org.elasticsearch.action.admin.indices.cache.clear.ClearIndicesCacheResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback;
 import org.elasticsearch.client.RestHighLevelClient;
+
 
 public class ElasticSearchClientForUITesting {
 	
@@ -18,11 +36,23 @@ public class ElasticSearchClientForUITesting {
 	private RestHighLevelClient esRestClient;
 	
 	
-	private ElasticSearchClientForUITesting (String hostURL, int port, String protcl) {
+	private ElasticSearchClientForUITesting (String hostURL, int port, String protcl, 
+			String userName, String password) {
 		
-		esRestClient = new RestHighLevelClient(
-		        RestClient.builder(
-		                new HttpHost(hostURL,port, protcl)));
+		if (protcl.equalsIgnoreCase("https")) {
+			
+			esRestClient = new RestHighLevelClient(
+					setUpHttpsClient(hostURL, port, userName, password)
+					);
+			
+		}
+		
+		else {
+			esRestClient = new RestHighLevelClient(
+			        RestClient.builder(
+			                new HttpHost(hostURL,port, protcl)));
+		}
+
 	}
 	
 
@@ -36,7 +66,7 @@ public class ElasticSearchClientForUITesting {
 //		return esClient;
 //	}
 	
-	public static ElasticSearchClientForUITesting getEsClient(String elasticSearchUrl) {
+	public static ElasticSearchClientForUITesting getEsClient(String elasticSearchUrl, String userName, String password) {
 	
 		URIBuilder uriBuilder = null;
 		try {
@@ -45,18 +75,17 @@ public class ElasticSearchClientForUITesting {
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		} 
-		
-		
-	if (esClient == null) {
-		esClient = new ElasticSearchClientForUITesting
-				(uriBuilder.getHost(), 
-				 uriBuilder.getPort(), 
-				 uriBuilder.getScheme());
-	}
+
+		if (esClient == null) {
+			esClient = new ElasticSearchClientForUITesting
+					(uriBuilder.getHost(), 
+					 uriBuilder.getPort(), 
+					 uriBuilder.getScheme(), userName, password);
+		}
 	
 	return esClient;
 }
-	
+		
 	public ClearIndicesCacheResponse clearESCache () {
 		
 		ClearIndicesCacheRequest request = new ClearIndicesCacheRequest();
@@ -78,5 +107,59 @@ public class ElasticSearchClientForUITesting {
 		
 		return clearCacheResponse;
 	}
+	
+	public static class TrustAllX509TrustManager implements X509TrustManager {
+		public X509Certificate[] getAcceptedIssuers() {
+			return new X509Certificate[0];
+		}
 
+		public void checkClientTrusted(java.security.cert.X509Certificate[] certs,
+									   String authType) {
+		}
+
+		public void checkServerTrusted(java.security.cert.X509Certificate[] certs,
+									   String authType) {
+		}
+
+	}
+	
+	
+	public RestClientBuilder setUpHttpsClient(String hostUrl, int port, String userName, String password) {
+		
+		String ES_HOST = hostUrl;
+		String ESProtocol = "https";
+		int es_port = port;
+		RestClientBuilder builder = null;
+		
+		try {
+			final SSLContext sc = SSLContext.getInstance("TLS");
+			sc.init(null, new TrustManager[]{new TrustAllX509TrustManager()}, new java.security.SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+			HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+				public boolean verify(String string, SSLSession ssls) {
+					return true;
+				}
+			});
+
+			final CredentialsProvider credentialsProvider =
+					new BasicCredentialsProvider();
+			credentialsProvider.setCredentials(AuthScope.ANY,
+					new UsernamePasswordCredentials(userName, password));
+
+			builder = RestClient.builder(
+					new HttpHost(ES_HOST, es_port, ESProtocol)).setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+				@Override
+				public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+					httpClientBuilder.setSSLHostnameVerifier((s, sslSession) -> true);
+					httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+					return httpClientBuilder.setSSLContext(sc);
+				}
+			});
+		} catch (KeyManagementException | NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return builder;
+	}
 }
