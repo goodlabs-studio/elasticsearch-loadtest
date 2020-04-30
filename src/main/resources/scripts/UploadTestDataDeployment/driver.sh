c2d947e841a6ac8dbc7329d80d8139f51a518e9f
rliugoodlabs <richard24@gmail.com>
2020-04-30 12:04:09 -0400

    updated driver script to take in number of nodes to determine whether to turn replication back on or not, and parameterized "base file"  to use

--- src/main/resources/scripts/UploadTestDataDeployment/driver.sh
+++ src/main/resources/scripts/UploadTestDataDeployment/driver.sh
@@ -4,6 +4,9 @@ URL=${2}
 STARTDAY=${3}
 ENDDAY=${4}
 DOCPERPERIOD=${5}
+BASEDOCA=${6}
+BASEDOCB=${7}
+NUMNODES=${8}
 
 TIMESTAMPTAG="REPLACE_ME"
 
@@ -13,14 +16,16 @@ echo ${START}
 for (( DAY=${STARTDAY}; DAY<=${ENDDAY}; DAY++ ))
 do
 
-awk 'NR % 2 == 0' timestamps.txt | xargs -P${1} -I % ./generateAndUploadInd.sh % "data-%" "${DOCPERPERIOD}" "baseA.json" "${TIMESTAMPTAG}" "${URL}" "${DAY}"
+awk 'NR % 2 == 0' timestamps.txt | xargs -P${1} -I % ./generateAndUploadInd.sh % "data-%" "${DOCPERPERIOD}" "${BASEDOCA}" "${TIMESTAMPTAG}" "${URL}" "${DAY}"
 wait
-awk 'NR % 2 == 1' timestamps.txt | xargs -P${1} -I % ./generateAndUploadInd.sh % "data-%" "${DOCPERPERIOD}" "baseB.json" "${TIMESTAMPTAG}" "${URL}" "${DAY}"
+awk 'NR % 2 == 1' timestamps.txt | xargs -P${1} -I % ./generateAndUploadInd.sh % "data-%" "${DOCPERPERIOD}" "${BASEDOCB}" "${TIMESTAMPTAG}" "${URL}" "${DAY}"
 wait
+if [ $NUMNODES -gt 1 ]
+then
 ./restoreReplica.sh "${DAY}"
+fi
 done
 
 END=$(($(date +%s%N)/1000000))
 echo ${END}
 echo "took " $(($END-$START)) "ms"
-
